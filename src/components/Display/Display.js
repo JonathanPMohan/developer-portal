ac740b2fce04b8cb985aeae083273dff5ea9cbf0
Jonathan Mohan <JonathanMohan.net@gmail.com>
2018-12-17 16:09:11 -0600

    built dashboard display component

---
+++ src/components/Display/Display.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Display.scss';
+
+class Form extends React.Component {
+  render() {
+    return (
+      <div className="form">
+      </div>
+    );
+  }
+}
+
+export default Form;
