75769c9d473195fc017586b526025532c139f3e8
Jonathan Mohan <JonathanMohan.net@gmail.com>
2018-12-18 15:43:36 -0600

    added dummy data, changed setup

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,57 +1,50 @@
-import React from 'react';
-import {
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  Col,
-  Button,
-} from 'reactstrap';
 
+import React from 'react';
 import './Form.scss';
 
 class InputForm extends React.Component {
   render() {
     return (
-      <div id="userInput">
-        <Form id="userInput">
-          <FormGroup>
-            <Input type="name" name="name" id="userName" placeholder="What's In A Name?" />
-            <Label for="exampleLink"></Label>
-            <Input type="link" name="link" id="itemLink" placeholder="WWW.Drop That Link" />
-          </FormGroup>
-          <div className="col-3 radio-buttons">
-            <FormGroup check>
-              <Label check>
-                <Input type="radio" name="tutorial" />{' '}
-                Tutorial
-            </Label>
-            </FormGroup>
-            <FormGroup check>
-              <Label check>
-                <Input type="radio" name="blog" />{' '}
-                Blog
-            </Label>
-            </FormGroup>
-            <FormGroup check>
-              <Label check>
-                <Input type="radio" name="podcast" />{' '}
-                Podcast
-            </Label>
-            </FormGroup>
-            <FormGroup check>
-              <Label check>
-                <Input type="radio" name="resource" />{' '}
-                Resources
-            </Label>
-            </FormGroup>
+      <div className="userInput row">
+        <div className="form col-8 mt-2">
+          <div className="col-auto">
+            <div className="input-group mb-2">
+              <div className="input-group-prepend">
+              </div>
+              <input type="text" className="userName form-control" id="name-input" placeholder="What's In A Name?"></input>
+            </div>
           </div>
-          <FormGroup check row>
-            <Col sm={{ size: 10, offset: 2 }}>
-              <Button>Submit</Button>
-            </Col>
-          </FormGroup>
-        </Form>
+          <div className="col-auto">
+            <div className="input-group mb-2">
+              <div className="input-group-prepend">
+              </div>
+              <input type="text" className="itemLink form-control" id="link-input" placeholder="Drop That Link!"></input>
+            </div>
+          </div>
+        </div>
+        <div className="col-3 radio-buttons">
+          <div className="form-check">
+            <input className="form-check-input" type="radio" name="tutorialRadio" id="tutorial-radio" value="option1"></input>
+            <label className="form-check-label">Tutorial</label>
+          </div>
+          <div className="form-check">
+            <input className="form-check-input" type="radio" name="blogRadio" id="blog-radio" value="option1"></input>
+            <label className="form-check-label">Blog</label>
+          </div>
+          <div className="form-check">
+            <input className="form-check-input" type="radio" name="resourceRadio" id="resource-radio" value="option1"></input>
+            <label className="form-check-label">Resource</label>
+          </div>
+          <div className="form-check">
+            <input className="form-check-input" type="radio" name="podcastRadio" id="podcast-radio" value="option1"></input>
+            <label className="form-check-label">Podcast</label>
+          </div>
+        </div>
+        <div id="submitButton">
+          <button type="submit" className="btn btn-alert add-btn">
+            <i className="fas fa-plus-circle icon-large"></i>
+          </button>
+        </div>
       </div>
     );
   }
