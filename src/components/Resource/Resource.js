c062ac6a565a0a54bfed45103e15192e5e647595
Jonathan Mohan <JonathanMohan.net@gmail.com>
2018-12-21 20:54:34 -0600

    finally got my info printing inside the tab pane

--- src/components/Resource/Resource.js
+++ src/components/Resource/Resource.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
 import './Resource.scss';
 
 class Resource extends React.Component {
   render() {
     const { resource } = this.props;
     return (
-      <h2>{resource.name}</h2>
+      <div className="resources">
+        <h2>{resource.name}</h2>
+      </div>
     );
   }
 }
